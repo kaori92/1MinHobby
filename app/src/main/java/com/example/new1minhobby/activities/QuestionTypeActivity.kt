20885ae7076961f95c8b239b5ee83a9a480bf04c
kaori92 <kaori92@gmail.com>
2020-02-24 22:29:50 +0100

    Add launcher icon

--- app/src/main/java/com/example/new1minhobby/activities/QuestionTypeActivity.kt
+++ app/src/main/java/com/example/new1minhobby/activities/QuestionTypeActivity.kt
@@ -6,11 +6,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.example.new1minhobby.R
+import com.google.android.gms.ads.MobileAds
 
 class QuestionTypeActivity : Activity() {
     protected override fun onCreate(savedInstanceState : Bundle?) {
         setTheme(R.style.splashScreenTheme)
         super<Activity>.onCreate(savedInstanceState)
+
+        MobileAds.initialize(this) {}
         setContentView(R.layout.question_type)
     }
 
