9b4d324e43551baad9f3e9a09e0c22c54bb16482
kaori92 <kaori92@gmail.com>
2020-03-01 20:15:57 +0100

    working gif but with white frame

--- app/src/main/java/com/example/new1minhobby/Constants.kt
+++ app/src/main/java/com/example/new1minhobby/Constants.kt
@@ -1,4 +1,4 @@
 package com.example.new1minhobby
 
 val DELIMITER = "\\t".toRegex()
-const val SPLASH_DISPLAY_LENGTH_MS: Long = 3000
+const val SPLASH_DISPLAY_LENGTH_MS: Long = 2000
--- app/src/main/java/com/example/new1minhobby/activities/SplashActivity.kt
+++ app/src/main/java/com/example/new1minhobby/activities/SplashActivity.kt
@@ -1,54 +1,28 @@
 package com.example.new1minhobby.activities
 
 import android.app.Activity
-import android.graphics.drawable.AnimationDrawable
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
 import android.widget.ImageView
-import androidx.core.os.postDelayed
+import com.bumptech.glide.Glide
 import com.example.new1minhobby.R
-
+import com.example.new1minhobby.SPLASH_DISPLAY_LENGTH_MS
 
 class SplashActivity : Activity() {
-    private lateinit var animation: AnimationDrawable
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        
-        val imageView =
-            findViewById<View>(R.id.imageView2) as ImageView // Declare an imageView to show the animation.
-        imageView.setBackgroundResource(R.drawable.splash_screen_animation)
-        animation = imageView.background as AnimationDrawable
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        Glide.with(this).asGif().load(R.raw.splash).into(imageView)
+
+        Handler().postDelayed(this::startNextActivity, SPLASH_DISPLAY_LENGTH_MS)
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        animation.start()
-
-//        val intent = Intent(this, QuestionTypeActivity::class.java)
-        checkAnimationStatus(50, animation)
-    }
-
-    /**
-     * check the animation status recursively, keep the animation until it reach the last frame.
-     *
-     * @param time              period of animation
-     * @param animationDrawable animation list
-     */
-    private fun checkAnimationStatus(
-        time: Int,
-        animationDrawable: AnimationDrawable
-    ) {
-        val handler = Handler()
-        handler.postDelayed(Runnable {
-            if (animationDrawable.current !== animationDrawable.getFrame(animationDrawable.numberOfFrames - 1)) checkAnimationStatus(
-                time,
-                animationDrawable
-            ) else finish()
-        }, time.toLong())
+    private fun startNextActivity(){
+        val intent = Intent(this, QuestionTypeActivity::class.java)
+        startActivity(intent)
     }
 }
